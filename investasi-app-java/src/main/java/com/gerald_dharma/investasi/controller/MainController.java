16cac8cc78a54fafc3608cd7f81e6b1255691237
iprdy <iputuradityadharmayoga@gmail.com>
2025-04-16 22:48:52 +0800

    Enter uname,pass

--- investasi-app-java/src/main/java/com/gerald_dharma/investasi/controller/MainController.java
+++ investasi-app-java/src/main/java/com/gerald_dharma/investasi/controller/MainController.java
@@ -1,8 +1,10 @@
 package com.investasi.controller;
 
+import com.investasi.ui.MainMenu;
+
 public class MainController {
     public void start() {
-        // Panggil menu utama (nanti akan dibuat)
+        MainMenu.show();
     }
 }
 
--- investasi-app-java/src/main/java/com/gerald_dharma/investasi/ui/MainMenu.java
+++ investasi-app-java/src/main/java/com/gerald_dharma/investasi/ui/MainMenu.java
@@ -6,6 +6,9 @@ public class MainMenu {
     public static void show() {
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter username : "); String username = sc.nextLine();
+        System.out.print("Enter password : "); String password = sc.nextLine();
+
 
     }
 }
