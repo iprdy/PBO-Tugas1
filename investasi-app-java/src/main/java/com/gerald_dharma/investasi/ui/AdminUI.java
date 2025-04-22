13059ed440ba4b83f7884ff2f8975546f2ebde5f
iprdy <iputuradityadharmayoga@gmail.com>
2025-04-22 23:26:19 +0800

    Memindahkan seluruh ui dari class MenuAdmin ke AdminUI

---
+++ investasi-app-java/src/main/java/com/gerald_dharma/investasi/ui/AdminUI.java
@@ -0,0 +1,23 @@
+package com.investasi.ui;
+
+public class AdminUI {
+    public static void mainMenu() {
+        System.out.println("Menu Admin");
+        System.out.println("1. Saham");
+        System.out.println("2. SBN");
+        System.out.println("3. Logout");
+    }
+
+    public static void menuSaham() {
+        System.out.println("Menu Admin > Saham");
+        System.out.println("1. Tambah Saham");
+        System.out.println("2. Ubah Harga Saham");
+        System.out.println("3. Kembali");
+    }
+
+    public static void menuSBN() {
+        System.out.println("Menu Admin > SBN");
+        System.out.println("1. Tambah SBN");
+        System.out.println("2. Kembali");
+    }
+}
